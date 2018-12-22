18e804362b93c2821f63eb8fa0ed06c465a4a7ca
Wyro <tomasz.wyrobek@hotmail.com>
2018-12-22 20:00:13 +0100

    Alfa version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Player;
+    public bool StartGame = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player.GetComponent<Rigidbody2D>().simulated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0 || StartGame)
+        {
+            Player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float Force = 1;
     public float MyVelocity = 1;
     public Rigidbody2D myRigidbody;
+    public GameObject EndingUI;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        this.myRigidbody.simulated = true;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         MovePlayer();
         AddForce();
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("You died");
         Die();
+        EndingUI.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EndingUI.SetActive(true);
     }
 
     public void Die()
     {
+        Debug.Log("You died");
         this.gameObject.SetActive(false);
     }
 
